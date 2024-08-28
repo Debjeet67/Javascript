8e0d35dc3f21a0fcccc76b4056dc2c5242be69d0
Debjeet67 <debjeetsarkar67@gmail.com>
2024-08-28 23:49:25 +0530

    Nums_and_Maths

--- 07_Nums and Maths.js
+++ 07_Nums and Maths.js
@@ -7,10 +7,41 @@ console.log(newScore);
 console.log(newScore.toString().length);*/
 //Now the number has converted to string and we know that in string we can check the length of the number
 
-const number = 98.3695
-console.log(number.toPrecision(4));
+/*const number = 98.3695
+console.log(number.toPrecision(4));*/
 //Gives a precise value and the number in the bracket is used for after how many digits it will give a precise value
 
-const hundreds = 10000000000
-console.log(hundreds.toLocaleString('en-In'));
+/*const hundreds = 10000000000
+console.log(hundreds.toLocaleString('en-In'));*/
 //It is used to give commas between large numbers to identify them easily
+
+//*******************Maths****************************/
+
+ /*console.log(Math.round(6.8));
+ //It is used to print a number in round figure
+
+ console.log(Math.ceil(5.1));
+ //It is used to print a number in top round value.
+
+ console.log(Math.floor(5.1));
+ //It is used to print a number in lower round value.
+
+ console.log(Math.min(8,5,78,95,74));
+ console.log(Math.max(8,5,78,95,74));*/
+
+ console.log(Math.random());
+ //It gives a value between 0 to 1
+ console.log(Math.random()*10 +1);
+ //It gives a value between 0 to 1 after multiplying it by 10
+ console.log(Math.floor(Math.random()*10) +1);
+  //It gives a value between 0 to 1 after multiplying it by 10 and the value is a lower value
+
+  const max = 20
+  const min = 10
+  console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+  
+
+
+
+ 
