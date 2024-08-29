13d423dd91f2afc755f28c1a4a3fe5c6d5f799bb
Debjeet67 <debjeetsarkar67@gmail.com>
2024-08-29 10:27:03 +0530

    Dates

---
+++ 08_dates.js
@@ -0,0 +1,12 @@
+/*let date = new Date()
+console.log(date.toString());
+//It is used to convert the date to string
+console.log(date.toDateString());
+//It is used to show only todays date
+console.log(date.toLocaleString());
+//It is used to show todays date and current time
+console.log(typeof date);*/
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
